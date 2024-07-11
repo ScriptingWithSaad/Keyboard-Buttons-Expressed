e65c4798436556d010b886f622742915aed82a19
Scripting With Saad <muhammadsaad965000@gmail.com>
2024-07-11 10:20:34 +0500

    adding script folder

---
+++ script/script.js
@@ -0,0 +1,20 @@
+let insert = document.querySelector('.insert');
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+        <div class="table-time">
+            <table>
+                <tr>
+                    <th>Key</th>
+                    <th>KeyCode</th>
+                    <th>Code</th>
+                </tr>
+                <tr>
+                    <td>${e.key === ' ' ? 'SPACE' : e.key}</td>
+                    <td>${e.keyCode}</td>
+                    <td>${e.code}</td>
+                </tr>
+            </table>
+        </div>
+    `;
+});
